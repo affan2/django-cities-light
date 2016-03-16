74d718b19ec49c0ca4c724533af1ec725003adef
Ahmed Salman <ahmed.salman.noor@gmail.com>
2016-03-16 20:32:03 +0500

    remove emtry transtion of region

---
+++ cities_light/management/commands/region_missing_translations.py
@@ -0,0 +1,13 @@
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        from cities_light import Region
+        for item in Region.published.all():
+            if not item.translations.all():
+                obj = item.translate(item.default_language)
+                obj.name = "no region name given"
+                obj.display_name = "no region name given"
+                obj.save()
