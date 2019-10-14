1eb770a6998aaa2f4b43a8f3ee2e1e31d3398ee0
Mohammad Abouchama <mohammadabouchama@gmail.com>
2019-10-14 16:19:12 +0400

    Fix registration of autocomplete views

--- cities_light/urls.py
+++ cities_light/urls.py
@@ -1,9 +1,21 @@
-from django.conf.urls import re_path
+from django.urls import re_path, path
 
 from .autocomplete_views import *
 
 urlpatterns = [
-    re_path(r'^city-autocomplete/$', CityAutocomplete.as_view(), name='city-autocomplete', ),
-    re_path(r'^region-autocomplete/$', RegionAutocomplete.as_view(), name='region-autocomplete', ),
-    re_path(r'^country-autocomplete/$', CountryAutocomplete.as_view(), name='country-autocomplete', ),
+    path(
+        'city-autocomplete',
+        CityAutocomplete.as_view(),
+        name='city-autocomplete',
+    ),
+    path(
+        'region-autocomplete',
+        RegionAutocomplete.as_view(),
+        name='region-autocomplete',
+    ),
+    path(
+        'country-autocomplete',
+        CountryAutocomplete.as_view(),
+        name='country-autocomplete',
+    ),
 ]
